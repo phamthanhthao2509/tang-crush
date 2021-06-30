e7289ca84a7febd2a3acf16fa1174d9570780e2f
phamthanhthao2509 <86727623+phamthanhthao2509@users.noreply.github.com>
2021-06-30 21:09:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,12 @@
 const CONFIG = {
     introTitle: 'Linh à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Khi cậu bước đến đã phá bỏ đinh luật của nemton
+    Vì vạn vật không còn hấp dẫn`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích tớ <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnNo: 'Không, Cậu à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu tớ <3',
     btnReply: 'Gửi cho mình <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
